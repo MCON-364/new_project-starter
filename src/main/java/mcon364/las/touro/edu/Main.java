c8655ecf6604acad213acbe8b66cd3cd39c7e4e0
Why3v3rN07 <hfogarty@student.touro.edu>
2026-02-03 17:30:09 -0500

    class ended

--- src/main/java/mcon364/las/touro/edu/Main.java
+++ src/main/java/mcon364/las/touro/edu/Main.java
@@ -1,2 +1,23 @@
 package mcon364.las.touro.edu;
+
+import java.util.Optional;
+
 //placeholder
+public class Main {
+    public static Optional<String> getUserName(String envVarName) {
+        return Optional.of(System.getenv(envVarName));
+    }
+
+    public static String getGreeting(String envVarName){
+        Optional<String> userName = getUserName(envVarName);
+        StringBuilder greeting = new StringBuilder();
+        greeting.append("Hello, ");
+        greeting.append(
+            switch (userName.isPresent()) {
+                case true -> yield userName.get();
+                default -> yield "stranger";
+            }
+        );
+        return greeting.toString();
+    }
+}

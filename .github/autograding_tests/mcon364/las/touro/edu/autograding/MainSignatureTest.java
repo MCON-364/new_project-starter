9c6b81daaeed76f77b133d8585ac469bdac0a34e
Larisa Agishtein <lagishtein@touro.edu>
2026-02-10 12:43:19 -0500

    Change return type assertion for processValues method

--- .github/autograding_tests/mcon364/las/touro/edu/autograding/MainSignatureTest.java
+++ .github/autograding_tests/mcon364/las/touro/edu/autograding/MainSignatureTest.java
@@ -129,7 +129,7 @@ public class MainSignatureTest {
 
         assertTrue(Modifier.isStatic(m.getModifiers()), "processValues must be static");
         assertTrue(Modifier.isPublic(m.getModifiers()), "processValues must be public");
-        assertEquals(Integer.class, m.getReturnType(), "processValues must return Integer");
+        assertEquals(int.class, m.getReturnType(), "processValues must return Integer");
     }
 
     /**
